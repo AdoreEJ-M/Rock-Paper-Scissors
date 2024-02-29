de3db65a4e1662313a925f7f0ba6ae443a54ae3d
Ethan Martinez <149119742+AdoreEJ-M@users.noreply.github.com>
2024-02-28 19:33:34 -0600

    create function, gets computer choice

---
+++ script.js
@@ -0,0 +1,19 @@
+// Allows computer to choose R,P, or S
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * 3);
+
+    switch(computerChoice) {
+        case 0:
+            computerChoice = 'rock';
+            break;
+        
+        case 1:
+            computerChoice = 'paper';
+            break;
+
+        case 2:
+            computerChoice = 'scissors';
+            break;
+    }
+    return computerChoice;
+}
